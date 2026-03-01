6af594e4f6eddc390a53b8d6024da3eb25a00900
Bill Klinkatsis <billklinkatsis@gmail.com>
2026-02-28 19:20:41 -0800

    “first_combine_results1.29”

--- football_prophesy/combine_results.py
+++ football_prophesy/combine_results.py
@@ -15,9 +15,21 @@ actual_combine_results = {
         "receivers_three_cone": {1: [], 2: [], 3: []}
     },
     "Tight Ends": {
-        "ends_40_yard": {1: ["Kenyon Sadiq"], 2: ["RJ Maryland", "Eli Stowers"], 3: []},
-        "ends_bench_press": {1: ["Matthew Hibner"], 2: ["Kenyon Sadiq"], 3: ["Khalil Dinkins", "Sam Roush"]},
-        "ends_three_cone": {1: ["Sam Roush"], 2: ["Khalil Dinkins"], 3: ["Josh Cuevas"]}
+        "ends_40_yard": {
+            1: ["Kenyon Sadiq"],
+            2: ["RJ Maryland", "Eli Stowers"],
+            3: []
+        },
+        "ends_bench_press": {
+            1: ["Matthew Hibner"],
+            2: ["Kenyon Sadiq"],
+            3: ["Khalil Dinkins", "Sam Roush"]
+        },
+        "ends_three_cone": {
+            1: ["Sam Roush"],
+            2: ["Khalil Dinkins"],
+            3: ["Josh Cuevas"]
+        }
     },
     "Offensive Linemen": {
         "linemen_40_yard": {1: [], 2: [], 3: []},
@@ -25,19 +37,55 @@ actual_combine_results = {
         "linemen_three_cone": {1: [], 2: [], 3: []}
     },
     "Defensive Linemen": {
-        "linemen_40_yard": {1: ["David Bailey"], 2: ["Malachi Lawrence"], 3: ["Trey Moore"]},
-        "linemen_bench_press": {1: ["David Gusta"], 2: ["Gabe Jacas", "Damonic Williams"], 3: []},
-        "linemen_three_cone": {1: ["Dani Dennis-Sutton"], 2: ["George Gumbs Jr."], 3: ["Albert Regis"]}
+        "linemen_40_yard": {
+            1: ["David Bailey"],
+            2: ["Malachi Lawrence"],
+            3: ["Trey Moore"]
+        },
+        "linemen_bench_press": {
+            1: ["David Gusta"],
+            2: ["Gabe Jacas", "Damonic Williams"],
+            3: []
+        },
+        "linemen_three_cone": {
+            1: ["Dani Dennis-Sutton"],
+            2: ["George Gumbs Jr."],
+            3: ["Albert Regis"]
+        }
     },
     "Linebackers": {
-        "40_yard": {1: ["Sonny Styles", "Arvell Reese"], 2: [], 3: ["Kaleb Elarms-Orr"]},
-        "bench_press": {1: ["Josiah Trotter"], 2: ["Taurean York"], 3: ["Keyshaun Elliott"]},
-        "three_cone": {1: ["Jacob Rodriguez"], 2: ["Kyle Louis"], 3: ["Jimmy Rolder"]}
+        "40_yard": {
+            1: ["Sonny Styles", "Arvell Reese"],
+            2: [],
+            3: ["Kaleb Elarms-Orr"]
+        },
+        "bench_press": {
+            1: ["Josiah Trotter"],
+            2: ["Taurean York"],
+            3: ["Keyshaun Elliott"]
+        },
+        "three_cone": {
+            1: ["Jacob Rodriguez"],
+            2: ["Kyle Louis"],
+            3: ["Jimmy Rolder"]
+        }
     },
     "Defensive Backs": {
-        "backs_40_yard": {1: ["Domani Jackson"], 2: ["Davison Igbinosun"], 3: ["Daylen Everette"]},
-        "backs_bench_press": {1: ["A.J. Haulcy"], 2: ["Xavier Nwankpa"], 3: ["Jalon Kilgore"]},
-        "backs_three_cone": {1: ["D'Angelo Ponds"], 2: ["Avieon Terrell"], 3: ["Hezekiah Masses"]}
+        "backs_40_yard": {
+            1: ["Lorenzo Styles Jr."],
+            2: ["Toriano Pride Jr.", "Robert Spears-Jennings"],
+            3: []
+        },
+        "backs_bench_press": {
+            1: ["Dillon Thieneman"],
+            2: ["Chris Johnson"],
+            3: ["Avieon Terrell"]
+        },
+        "backs_three_cone": {
+            1: ["Latrell McCutchin Sr."],
+            2: ["Avery Smith"],
+            3: ["Julian Neal"]
+        }
     },
     "Specialists": {
         "40_yard": {1: [None], 2: [None], 3: [None]},
