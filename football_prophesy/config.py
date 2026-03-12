e9aa966a98d732aff84bfd3140f76b5ba730b907
Bill Klinkatsis <billklinkatsis@gmail.com>
2026-03-11 22:52:35 -0700

    “structurecleanup3.01”

--- football_prophesy/config.py
+++ football_prophesy/config.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///football_prophesy.db"
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/football_prophesy.db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
